ddc06f64d71eb3783dcd72be743b18a222f70154
Nevine Achola <nevine.achola@student.moringaschool.com>
2023-03-15 12:18:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const num3 = 20;
+const num4 = 16;
+
+let multiply = num1*num2;
+
+
+
+let random = Math.floor(Math.random() * 15) + 1;
+
+let mod = num3 % num4;
+
+const max = Math.max(12, 20);
+
